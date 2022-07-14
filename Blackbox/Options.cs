eb76c33320af567e850401aff21e72fa9e685db3
rbo13 <chardyy.orofeo@gmail.com>
2022-07-14 20:17:17 +0800

    added Options class to setup blaggo options

---
+++ Blackbox/Options.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaggoBlackbox
+{
+    public class Options
+    {
+        public string AuthURL;
+        public Credentials Credentials;
+        public Authenticator AuthenticatorFn;
+        public HttpClient HttpClient;
+    }
+
+    public class Credentials
+    {
+        public string Username;
+        public string Password;
+    }
+
+    public delegate Task<AuthResponse> Authenticator(string url, Credentials creds);
+}
